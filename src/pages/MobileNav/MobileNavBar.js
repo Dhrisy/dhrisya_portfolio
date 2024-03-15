59632c2ca2ec6d723f46f8d1891b7d0ffc5e32a7
Dhrisy <ddhrisyal2000@gmail.com>
2024-03-15 13:26:53 +0530

    add action to mobile nav bar

--- src/pages/MobileNav/MobileNavBar.js
+++ src/pages/MobileNav/MobileNavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./MobileNavBar.css"
+import { Link } from 'react-scroll';
 
 
 const MobileNavBar = ({isOpen, toggleMenu}) => {
@@ -11,11 +12,38 @@ const MobileNavBar = ({isOpen, toggleMenu}) => {
     >
         
         <div className='mobile-menu-container'>
-        <span className='Mobilenav-bar-item'>Home</span>
-           <span className='Mobilenav-bar-item'>Skills</span>
-           <span className='Mobilenav-bar-item'>Experience</span>
-           <span className='Mobilenav-bar-item'>Contact me</span>
-           <button className='Mobilenav-bar-btn' onClick={() => {}}>Hire me</button>
+        <Link
+          activeClass='active' to='hero-section'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+           className='Mobilenav-bar-item'>Home</Link>
+           <Link 
+          activeClass='active' to='skill-section'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className='Mobilenav-bar-item'>Skills</Link>
+           <Link
+          activeClass='active' to='experience-section'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className='Mobilenav-bar-item'>Experience</Link>
+           <Link 
+          activeClass='active' to='contact-section'
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className='Mobilenav-bar-item'>Contact me</Link>
+           <button className='Mobilenav-bar-btn' onClick={() => {
+            // isOpen = false;
+            document.getElementById("contact-section").scrollIntoView({behavior: "smooth"});
+           }}>Hire me</button>
         </div>
 
     </div>
